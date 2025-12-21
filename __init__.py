8a12160ddda1301921ce140367e5f11bab3211a4
artstich <art.hw@petalmail.com>
2025-12-22 07:50:35 +0800

    🔥NodeAlignPro版本大更新,优化文件结构(全新UI, 更全的节点上色系统)→详见readme文档

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@ import os
 NODE_CLASS_MAPPINGS = {}
 current_directory = os.path.dirname(os.path.abspath(__file__))
 WEB_DIRECTORY = os.path.join(current_directory)
-# NODE_JS = ["ArtsticH/hNodeAlignPro.js"]
+NODE_JS = ["ArtsticH/hNodeAlignPro.js"]
