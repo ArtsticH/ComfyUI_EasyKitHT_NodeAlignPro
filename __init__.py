64a15843051fe2b1264339320c4df28b7e1fa30e
artstich <art.hw@petalmail.com>
2025-12-30 06:10:27 +0800

    fix version profile, pyproject.toml

--- __init__.py
+++ __init__.py
@@ -3,4 +3,5 @@ NODE_CLASS_MAPPINGS = {}
 current_directory = os.path.dirname(os.path.abspath(__file__))
 WEB_DIRECTORY = os.path.join(current_directory)
 NODE_JS = ["ArtsticH/hNodeAlignPro.js"]
-VERSION = "v2.0.3_rc"
+__VERSION__ = '2.0.3'
+VERSION = "2.0.3"
